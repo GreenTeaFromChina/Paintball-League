87066907d7e46fc6f6ebfc5b94b735f4214fa28b
Austin <achurdle9@yahoo.com>
2015-07-17 22:06:57 -0500

    Unity Main Project 7/17/2015

---
+++ PROJECT WTF/Assets/NetworkManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	public Camera standbyCamera;
+	SpawnSpot[] spawnSpots;
+	// Use this for initialization
+	void Start () {
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot> ();
+		Connect ();
+	}
+	
+	void Connect() {
+		PhotonNetwork.ConnectUsingSettings ("Project WTF SERVER");
+
+	}
+
+	void OnGUI() {
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+
+	void OnJoinedLobby() {
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed() {
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom() {
+		SpawnMyPlayer ();
+	}
+
+	void SpawnMyPlayer() {
+		if (spawnSpots == null) {
+			Debug.LogError ("WTF?!?!?!!?");
+			return;
+		}
+		SpawnSpot mySpawnSpot = spawnSpots [ Random.Range (0, spawnSpots.Length) ];
+		PhotonNetwork.Instantiate("FPSController", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 0);
+		standbyCamera.enabled = false;
+
+	}
+
+}
